4f172640a479c96b22a2d7772bcfa9519d9d64dd
aww <vovapodkorytov966@gmail.com>
2024-03-24 19:06:24 +0300

    Reading from System.in added

--- src/B/Main.java
+++ src/B/Main.java
@@ -6,51 +6,17 @@ import B.expression.Turnstile;
 import B.proof.*;
 import B.utils.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
-    private final static List<String> AXIOMES = List.of(
-            "A->B->A",
-            "(A->B)->(A->B->C)->(A->C)",
-            "A->B->A&B",
-            "A&B->A",
-            "A&B->B",
-            "A->A|B",
-            "B->A|B",
-            "(A->C)->(B->C)->(A|B->C)",
-            "(A->B)->(A->!B)->!A",
-            "!!A->A"
-    );
-
-    private final static Map<String, String> SUBSTITUTIONS = Map.of(
-            "A", "(P&Q)",
-            "B", "(W|Z)",
-            "C", "(!(S->T))"
-    );
-
-    private static void test() {
-        for (int i = 0; i < AXIOMES.size(); i++) {
-            String scheme = AXIOMES.get(i);
-            String ans = String.format("[Ax. sch. %d]", i + 1);
-            System.out.println("\nWas - " + scheme);
-            for (String variable : SUBSTITUTIONS.keySet()) {
-                scheme = scheme.replaceAll(variable, SUBSTITUTIONS.get(variable));
-            }
-            System.out.println("Now - " + scheme + "\n");
-            Expression expression = new ExpressionBuilder(scheme).parse();
-            System.out.println("Expression - " + expression);
-            AxiomeProof axProof = new AxiomeProof();
-            String res = axProof.check(expression);
-            System.out.printf("Correct - %s\nActual - %s%n", ans, res);
-        }
-    }
-
     public static void main(String[] args) {
-//        test();
-
         final Proof[] checks = new Proof[]{
                 new AxiomeProof(),
                 new HypothesesProof(),
@@ -58,32 +24,16 @@ public class Main {
                 new ModusPonensProof()
         };
 
-//        Scanner scanner = new Scanner(System.in);
-//        String line = scanner.next();
-//        Expression expression = new ExpressionBuilder(line).parse();
-//        for (Proof check : checks) {
-//            String verdict = check.check(expression);
-//            if (verdict != null) {
-//                System.out.println(verdict);
-//            }
-//        }
-
-        List<String> lines = List.of(
-//                "|-A&B->A",
-//                "|-A&B->B",
-//                "A&B|-A",
-//                "A&B|-B",
-//                "A&B|-B->A->B&A",
-//                "A&B|-A->B&A",
-//                "A&B|-B&A",
-//                "|-A&B->B&A"
-                "A->B,C,D|-E->D",
-                "A->B,C,D,D|-D",
-                "A->B,C,D,D|-D->E",
-                "A->B,C,D|-E",
-                "D,A->B,C,D|-E",
-                "E,D,C|-(A->B)->D"
-        );
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String line = reader.readLine();;
+            while (line != null) {
+                lines.add(line);
+                line = reader.readLine();
+            }
+        } catch (IOException ioException) {
+            System.out.println("Sadly :( " + ioException.getMessage());
+        }
 
         List<Expression> expressions = new ArrayList<>(
                 lines.stream()
